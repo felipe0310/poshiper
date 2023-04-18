a915bdc0a1fc305097c8e5621904794fb34a4ef3
felipe lopez <felipe.lopez.o@gmail.com>
2023-04-17 22:33:09 -0400

    nueva plantilla

---
+++ resources/views/layouts/theme/app.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Hiper Pañal - POS</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    @include('layouts.theme.styles')
+     @livewireStyles
+
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    
+</head>
+<body class="sidebar-noneoverflow">
+    
+    <!--  BEGIN NAVBAR  -->
+    @include('layouts.theme.navbar')
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN TOPBAR  -->
+        @include('layouts.theme.topbar')
+        <!--  END TOPBAR  -->
+        
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <!-- CONTENT AREA --> 
+
+                @yield('content')
+
+                <!-- CONTENT AREA -->
+
+            </div>
+            @include('layouts.theme.footer')
+        </div>
+        <!--  END CONTENT AREA  -->
+
+    </div>
+    <!-- END MAIN CONTAINER -->
+
+    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    @include('layouts.theme.scripts')
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/layouts/theme/footer.blade.php
@@ -0,0 +1,17 @@
+{{-- <div class="footer-wrapper">
+    <div class="footer-section f-section-1">
+        <p class="">Sistema Hiper Pos</p>
+    </div>
+    <div class="footer-section f-section-2">
+        <p class="">Versión 1.0.0 - by Felipe López</p>
+    </div>
+</div> --}}
+
+<div class="footer-wrapper">
+    <div class="footer-section f-section-1">
+        <p class="">Sistema Hiper Pañal - POS</p>
+    </div>
+    <div class="footer-section f-section-2">
+        <p class="">V 1.0.0 - by Felipe López</p>
+    </div>
+</div>
